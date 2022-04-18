de1169d6fe7592a91d917a94a85b7dc712287bf3
PJH <ghdlwnsgh12@naver.com>
2022-04-18 22:43:03 +0900

    Add bfs dfs

--- PJH/src/Main.kt
+++ PJH/src/Main.kt
@@ -1,5 +1,21 @@
-
+import BaekJoon.BaekJoon_1260_S2
 
 fun main() {
+    val exStr = "4 5 1\n" +
+            "1 2\n" +
+            "1 3\n" +
+            "1 4\n" +
+            "2 4\n" +
+            "3 4"
+
+    val exStr2 = "5 5 3\n" +
+            "5 4\n" +
+            "5 2\n" +
+            "1 2\n" +
+            "3 4\n" +
+            "3 1"
+    val baekjoon1260S2 = BaekJoon_1260_S2()
+
+    println(baekjoon1260S2.solution(exStr2))
 
 }
