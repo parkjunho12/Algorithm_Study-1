da7f89dc875778c5d350fd0fef4c6c5582e89cc9
HyeongminDU <parkhm0829@datau.co.kr>
2022-04-19 12:20:18 +0900

    baekjoon1260

--- AbelPark/baekjoon/1260.js
+++ AbelPark/baekjoon/1260.js
@@ -1,12 +1,13 @@
+// 입력값 세팅
 var fs = require("fs")
 var txt = fs.readFileSync("1260.txt").toString().split("\n")
 var raw = txt
 var data = []
-
 for (let i = 0; i < raw.length; i++) {
   data[i] = raw[i].replace("\r", "").split(" ").map(Number)
 }
 
+// 로직 풀이
 var V = data[0][2]
 var stack = [V]
 var result = [V]
@@ -20,56 +21,79 @@ data.splice(0, 1)
 data.forEach((D1) => {
   D1.sort()
 })
-// 2.2 1차원 배열간
+// 2-2 1차원 배열간
 data.sort((a, b) => a[0] + a[1] - (b[0] + b[1]))
 
-// stack.push(V)
-// result.push(V)
-
+// 2-3 2차원 배열 중첩 !제거 할필요 없음!
+// let data = data.filter((element, index) => {
+//   return (
+//     //1차원 배열에서는 indexOf를 사용했지만 다차원 배열에서는 안먹힘
+//     data.findIndex((item) => item[0] === element[0] && item[1] === element[1]) === index
+//   )
+// })
 function DFS(p) {
   for (let i = 0; i < data.length; i++) {
     if (data[i].includes(p)) {
       if (data[i].indexOf(p) === 0) {
-        result.push(data[i][1]), stack.push(data[i][1])
+        if (result.includes(data[i][1]) === false) {
+          result.push(data[i][1])
+          stack.push(data[i][1])
+        }
       } else {
-        result.push(data[i][0]), stack.push(data[i][0])
+        if (result.includes(data[i][0]) === false) {
+          result.push(data[i][0])
+          stack.push(data[i][0])
+        }
       }
       data.splice(i, 1)
       flag = true
-      console.log(stack)
-      console.log(result)
       break
-    } else {
-      console.log("여긴안오냐")
-      flag = false
     }
   }
   if (flag === false) {
-    console.log(flag)
     stack.pop()
   }
   if (stack.length !== 0) {
+    flag = false
     DFS(stack[stack.length - 1])
   }
 }
 
-DFS(V)
+function BFS(p) {
+  let i = 0
+  while (data.length > i) {
+    if (data[i].includes(p)) {
+      if (data[i].indexOf(p) === 0) {
+        if (result.includes(data[i][1]) === false) {
+          result.push(data[i][1])
+          stack.push(data[i][1])
+        }
+      } else {
+        if (result.includes(data[i][0]) === false) {
+          result.push(data[i][0])
+          stack.push(data[i][0])
+        }
+      }
+      data.splice(i, 1)
+      flag = true
+    } else {
+      i++
+    }
+  }
+  if (flag === false) {
+    stack.shift()
+  }
+  if (stack.length !== 0) {
+    flag = false
+    BFS(stack[0])
+  }
+}
 
-// for (let i = 0; i < data.length; i++) {
-//   if (data[i].includes(V)) {
-//     if (data[i].indexOf(V) === 0) {
-//       result.push(data[i][1])
-//       stack.push(data[i][1])
-//     } else {
-//       result.push(data[i][0])
-//       stack.push(data[i][0])
-//     }
-//     data.splice(i, 1)
-//     break
-//   }
-// }
+// DFS(V)
+BFS(V)
 
-console.log(data)
 console.log("정점의 번호" + V)
 console.log("스택" + stack)
 console.log("탐색순서" + result)
+
+console.log(result.toString().replace(",", " "))
