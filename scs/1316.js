076744098aa59ad1410a80bcbba83b572029dd69
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2022-04-20 21:16:45 +0900

    feat: first commit 1316, 2606 in 22-04-20

---
+++ scs/1316.js
@@ -0,0 +1,46 @@
+const readline = require("readline")
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
+
+let input = []
+
+rl.on("line", line => input.push(line))
+rl.on("close", () => groupWord(input))
+
+const groupWord = input => {
+	const caseCount = Number(input[0])
+	let countGroupWord = 0
+
+	for(let i = 1; i <= caseCount; i++) {
+		const word = input[i]
+		const letter = []
+		let flagGroupWord = true
+		console.log(`--- word ${word} ---`)
+
+		for(let j = 0; j < word.length; j++) {
+			if(letter.indexOf(word[j]) === -1) {
+				// letter 배열 안에 word 단어 하나도 포함 안하는 경우
+				letter.push(word[j])
+			} else {
+				console.log(word[j - 1], word[j], letter.indexOf(word[j]), letter.length - 1)
+				// letter 배열 안에 단어 있을때
+				// 단어 연속해서 안나오는 경우
+				if(letter.indexOf(word[j]) !== letter.length - 1) {
+					flagGroupWord = false
+					console.log("--- break ---")
+					break
+				}
+			}
+		}
+		console.log("--- letter ---")
+		console.log(letter)
+		
+		if(flagGroupWord) countGroupWord += 1
+	}
+
+	console.log("--- countGroupWord ---")
+	console.log(countGroupWord)
+}
