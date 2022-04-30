45aa8ee85a008c8eb68ce730246e028e2549ecb0
songcs0329 <54650110+songcs0329@users.noreply.github.com>
2022-04-30 13:40:32 +0900

    feat: update week3 9613

--- scs/study/week3/9613.js
+++ scs/study/week3/9613.js
@@ -10,17 +10,22 @@ let input = []
 rl.on("line", line => input.push(line))
 
 rl.on("close", () => {
-	const t = parseInt(input.shift())
-	for(let i = 0; i < t; i++) {
-		const caseArr = input[i].split(" ").map(num => parseInt(num))
-		const n = caseArr.shift()
-		const min = Math.min(...caseArr)
-		for(let j = 1; j < n; j++) {
-			console.log(i, j, min, caseArr[i], caseArr[j])
-			//checkGCD(min, caseArr[i], caseArr[j])
-			//continue
-		}
-	}
-	
+   const t = parseInt(input.shift())
+   for(let i = 0; i < t; i++) {
+      const caseArr = input[i].split(" ").map(num => parseInt(num))
+      const n = caseArr.shift()
+      const gcdArr = []
+
+      for(let j = 0; j < n; j++) {
+         for(let k = j + 1; k < n; k++) {
+            const gcd = getGCD(caseArr[j], caseArr[k])
+            gcdArr.push(gcd)
+         }
+      }
+
+      const gcdSum = gcdArr.reduce((acc, cur) => acc += cur, 0)
+      console.log(gcdSum)
+   }
 })
 
+const getGCD = (j, k) => (j % k === 0 ? k : getGCD(k, j % k))
