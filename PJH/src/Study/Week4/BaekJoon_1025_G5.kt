a58df0c9cb684b4173325090c6cf50370e5bbf25
PJH <ghdlwnsgh12@naver.com>
2022-05-08 10:36:53 +0900

    Add

--- PJH/src/Study/Week4/BaekJoon_1025_G5.kt
+++ PJH/src/Study/Week4/BaekJoon_1025_G5.kt
@@ -3,5 +3,21 @@ package Study.Week4
 import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
+    val N = nextInt()
+    val M = nextInt()
+    val numArr: Array<IntArray> = Array<IntArray>(N) {
+        IntArray(M) {
+            0
+        }
+    }
+
+
+    for (i in 0 until N) {
+        val row = nextInt().toString()
+        for (j in row.indices) {
+            numArr[i][j] = row[j].toString().toInt()
+        }
+    }
+
 
 }
